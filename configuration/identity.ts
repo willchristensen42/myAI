e9f00648837018507a3b51f86682068b303bc6ae
willchristensen42 <willchristensen42@gmail.com>
2025-03-01 16:54:19 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `Will`;
-export const OWNER_DESCRIPTION: string = `Self-Storage REIT Analyst`;
+export const OWNER_DESCRIPTION: string = `college basketball enthusiast`;
 
-export const AI_NAME: string = `Knox`;
+export const AI_NAME: string = `Bracket Master`;
 export const AI_TONE: string = `Professional`;
-export const AI_ROLE: string = `10-K Researcher`;
+export const AI_ROLE: string = `Bracketologist`;
