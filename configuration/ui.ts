4770ff5e437b119abaab3f2770eb74333c69ed67
willchristensen42 <willchristensen42@gmail.com>
2025-03-01 15:44:27 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `[CHAT HEADER]`;
+export const CHAT_HEADER: string = `Knox`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
-export const FOOTER_MESSAGE: string = `Customized AI for March Madness Domination`;
+export const FOOTER_MESSAGE: string = `Expert Analysis of Self-Storage REITs`;
 export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
+export const PAGE_TITLE: string = `Knox: Self-Storage REIT Guru`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
