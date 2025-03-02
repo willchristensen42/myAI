4e050ceb990267d2f4cfebb60de7f50a4d0f57ea
willchristensen42 <willchristensen42@gmail.com>
2025-03-01 23:20:46 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -4,7 +4,7 @@ export const CHAT_HEADER: string = ``;
 export const MESSAGE_PLACEHOLDER: string = `Thinking...`;
 export const FOOTER_MESSAGE: string = `Custom AI for March Madness Domination`;
 export const CLEAR_BUTTON_TEXT: string = `Start A Fresh Analysis`;
-export const PAGE_TITLE: string = `Bracket Bot`;
+export const PAGE_TITLE: string = `Bracket Master`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
