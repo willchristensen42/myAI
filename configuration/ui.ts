abac2cfa8992c500281745a51fb7699e7f96dec1
willchristensen42 <willchristensen42@gmail.com>
2025-03-01 23:21:39 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,6 +1,6 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = ``;
+export const CHAT_HEADER: string = `Bracket Master`;
 export const MESSAGE_PLACEHOLDER: string = `Thinking...`;
 export const FOOTER_MESSAGE: string = `Custom AI for March Madness Domination`;
 export const CLEAR_BUTTON_TEXT: string = `Start A Fresh Analysis`;
